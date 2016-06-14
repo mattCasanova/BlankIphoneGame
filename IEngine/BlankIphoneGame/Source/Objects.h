2b8ae63ea39bd967730acebcefccc60d84a4eeb4
Matt Casanova <lazersquad@gmail.com>
2016-06-14 14:48:29 +0900

    Changed game to portrait mode and made it a univeral app as well as defined a game object.

---
+++ IEngine/BlankIphoneGame/Source/Objects.h
@@ -0,0 +1,25 @@
+/******************************************************************************/
+/*
+ File:   Objects.h
+ Author: Matt Casanova
+ Email:  lazersquad@gmail.com
+ Date:   2016/06/15
+ 
+ 
+Header file that defines game objects and buttons.  For now the objects are
+ very simple and would need to be changed depending on the game.  Later I 
+ will change the object to be component based.
+ */
+/******************************************************************************/
+#ifndef Objects_h
+#define Objects_h
+
+#import "Vec2.h"
+
+
+typedef struct
+{
+  
+}Object;
+
+#endif /* Objects_h */
